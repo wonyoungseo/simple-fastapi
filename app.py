ff8e146a3b7d5772b5b3bfd902f5441535691384
lucaseo <lucaseo0147@gmail.com>
2023-02-24 14:57:52 +0900

    add actions.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
 @app.get("/")
 def hello_world():
-    return "hello_world_2"
+    return {"message": "Hello World"}
